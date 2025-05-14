020cf43ae04404c29ddd9cf7a9ae2be658027817
karthaze <akshatrathore.rathore@gmail.com>
2025-05-14 19:46:55 +0530

    vercel analytics removed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import { Footer } from './components/Footer/Footer';
 import { Loader } from './components/Loader/Loader';
 import { FilmGrain } from './components/FilmGrain/FilmGrain';
 import styles from './App.module.css';
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -25,7 +24,6 @@ function App() {
 
   return (
     <div className={styles.app}>
-      <Analytics/>
       {loading ? (
         <Loader />
       ) : (
