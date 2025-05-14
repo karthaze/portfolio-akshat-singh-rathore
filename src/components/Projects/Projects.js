25c3a022f5b905218223f32088c3017b44182958
karthaze <akshatrathore.rathore@gmail.com>
2025-05-14 17:39:57 +0530

    clickable project image

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -82,11 +82,13 @@ export const Projects = () => {
                   </div>
                 </div>
 
-                <div className={styles.projectImageContainer}>
-                  <div className={styles.filmFrameTop}></div>
-                  <img src={project.image} alt={project.title} className={styles.projectImage} />
-                  <div className={styles.filmFrameBottom}></div>
-                </div>
+                <a href={project.links.live} target="_blank" rel="noopener noreferrer" className={styles.linkWrapper}>
+                  <div className={styles.projectImageContainer}>
+                    <div className={styles.filmFrameTop}></div>
+                    <img src={project.image} alt={project.title} className={styles.projectImage} />
+                    <div className={styles.filmFrameBottom}></div>
+                  </div>
+                </a>
               </div>
             ))}
           </div>
